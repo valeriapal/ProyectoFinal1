4faa1d4ea4bb1b37fe19d102933b4b5db52fdecd
valeriapal <avpalacios@ucundinamarca.edu.co>
2021-09-22 20:15:17 -0500

    parte inicial proyecto, creación formulario para seleccionar la opción del taller.

--- ProyectoFinal1/Form1.Designer.cs
+++ ProyectoFinal1/Form1.Designer.cs
@@ -29,148 +29,42 @@ namespace ProyectoFinal1
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_modulo1 = new System.Windows.Forms.Button();
-            this.btn_modulo2 = new System.Windows.Forms.Button();
-            this.btn_modulo3 = new System.Windows.Forms.Button();
-            this.btn_modulo4 = new System.Windows.Forms.Button();
-            this.btn_modulo5 = new System.Windows.Forms.Button();
-            this.btn_modulo6 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.cbbopciones = new System.Windows.Forms.ComboBox();
+            this.lblopciones = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btn_modulo1
+            // cbbopciones
             // 
-            this.btn_modulo1.Location = new System.Drawing.Point(293, 21);
-            this.btn_modulo1.Name = "btn_modulo1";
-            this.btn_modulo1.Size = new System.Drawing.Size(94, 33);
-            this.btn_modulo1.TabIndex = 0;
-            this.btn_modulo1.Text = "Modulo 1";
-            this.btn_modulo1.UseVisualStyleBackColor = true;
-            this.btn_modulo1.Click += new System.EventHandler(this.button1_Click);
+            this.cbbopciones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbopciones.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.cbbopciones.FormattingEnabled = true;
+            this.cbbopciones.Location = new System.Drawing.Point(92, 45);
+            this.cbbopciones.Name = "cbbopciones";
+            this.cbbopciones.Size = new System.Drawing.Size(206, 23);
+            this.cbbopciones.TabIndex = 0;
+            this.cbbopciones.SelectedIndexChanged += new System.EventHandler(this.cbbopciones_SelectedIndexChanged);
             // 
-            // btn_modulo2
+            // lblopciones
             // 
-            this.btn_modulo2.Location = new System.Drawing.Point(293, 75);
-            this.btn_modulo2.Name = "btn_modulo2";
-            this.btn_modulo2.Size = new System.Drawing.Size(94, 33);
-            this.btn_modulo2.TabIndex = 1;
-            this.btn_modulo2.Text = "Modulo 2";
-            this.btn_modulo2.UseVisualStyleBackColor = true;
-            // 
-            // btn_modulo3
-            // 
-            this.btn_modulo3.Location = new System.Drawing.Point(293, 133);
-            this.btn_modulo3.Name = "btn_modulo3";
-            this.btn_modulo3.Size = new System.Drawing.Size(94, 33);
-            this.btn_modulo3.TabIndex = 2;
-            this.btn_modulo3.Text = "Modulo 3";
-            this.btn_modulo3.UseVisualStyleBackColor = true;
-            // 
-            // btn_modulo4
-            // 
-            this.btn_modulo4.Location = new System.Drawing.Point(293, 192);
-            this.btn_modulo4.Name = "btn_modulo4";
-            this.btn_modulo4.Size = new System.Drawing.Size(94, 33);
-            this.btn_modulo4.TabIndex = 3;
-            this.btn_modulo4.Text = "Modulo 4";
-            this.btn_modulo4.UseVisualStyleBackColor = true;
-            // 
-            // btn_modulo5
-            // 
-            this.btn_modulo5.Location = new System.Drawing.Point(293, 251);
-            this.btn_modulo5.Name = "btn_modulo5";
-            this.btn_modulo5.Size = new System.Drawing.Size(94, 36);
-            this.btn_modulo5.TabIndex = 4;
-            this.btn_modulo5.Text = "Modulo 5";
-            this.btn_modulo5.UseVisualStyleBackColor = true;
-            // 
-            // btn_modulo6
-            // 
-            this.btn_modulo6.Location = new System.Drawing.Point(293, 320);
-            this.btn_modulo6.Name = "btn_modulo6";
-            this.btn_modulo6.Size = new System.Drawing.Size(94, 33);
-            this.btn_modulo6.TabIndex = 5;
-            this.btn_modulo6.Text = "Modulo 6";
-            this.btn_modulo6.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 20);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Menor de Dos Números";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 88);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(161, 20);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Calcular área Triangulo";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(114, 146);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 20);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Cuenta Letras";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(110, 198);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(108, 20);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Invertir Palabra";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(137, 259);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 20);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Pangramas";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(144, 326);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(74, 20);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Ahorcado";
+            this.lblopciones.AutoSize = true;
+            this.lblopciones.Location = new System.Drawing.Point(92, 27);
+            this.lblopciones.Name = "lblopciones";
+            this.lblopciones.Size = new System.Drawing.Size(89, 15);
+            this.lblopciones.TabIndex = 1;
+            this.lblopciones.Text = "Elegir Opciones";
+            this.lblopciones.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 458);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btn_modulo6);
-            this.Controls.Add(this.btn_modulo5);
-            this.Controls.Add(this.btn_modulo4);
-            this.Controls.Add(this.btn_modulo3);
-            this.Controls.Add(this.btn_modulo2);
-            this.Controls.Add(this.btn_modulo1);
+            this.ClientSize = new System.Drawing.Size(463, 344);
+            this.Controls.Add(this.lblopciones);
+            this.Controls.Add(this.cbbopciones);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Controlador de Versiones";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,18 +72,8 @@ namespace ProyectoFinal1
 
         #endregion
 
-        private System.Windows.Forms.Button btn_modulo1;
-        private System.Windows.Forms.Button btn_modulo2;
-        private System.Windows.Forms.Button btn_modulo3;
-        private System.Windows.Forms.Button btn_modulo4;
-        private System.Windows.Forms.Button btn_modulo5;
-        private System.Windows.Forms.Button btn_modulo6;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbbopciones;
+        private System.Windows.Forms.Label lblopciones;
     }
 }
 
--- ProyectoFinal1/Form1.cs
+++ ProyectoFinal1/Form1.cs
@@ -17,10 +17,68 @@ namespace ProyectoFinal1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e) //BOTON PARA LLAMAR AL FORMULARIO 2 (MODULO 1 MENOR DE DOS NUMEROS)
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            Form formulario2 = new Form2();
-            formulario2.Show();
+            cbbopciones.Items.Add("Menor de 2 números");
+            cbbopciones.Items.Add("Calcula el Area de un Triángulo");
+            cbbopciones.Items.Add("Cuenta letras");
+            cbbopciones.Items.Add("Invertir palabra");
+            cbbopciones.Items.Add("Panagramas");
+            cbbopciones.Items.Add("Ahorcado");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbopciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string indice = cbbopciones.SelectedItem.ToString();
+
+            if (!string.IsNullOrEmpty(indice))
+            {
+                switch (indice)
+                {
+                    case "Menor de 2 números":
+                        //crear funcion
+                        break;
+                    case "Calcula el Area de un Triángulo":
+                        areaTriangulo();
+                        break;
+                    case "Cuenta letras":
+                        //crear funcion
+                        break;
+                    case "Invertir palabra":
+                        InvertirPalabra();
+                        break;
+                    case "Panagramas":
+                        //crear funcion
+                        break;
+                    case "Ahorcado":
+                        ahorcado();
+                        break;
+                }
+            }
+        }
+
+        private void ahorcado()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InvertirPalabra()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// funcion que se encarga de calcular el area de un triangulo.
+        /// </summary>
+        private void areaTriangulo()
+        {
+            
         }
     }
 }
